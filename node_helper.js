b3ad696556893f9cf6c20df4efeda2bf7972565a
Chris van Marle <qistoph@gmail.com>
2016-12-06 19:36:38 +0100

    Async callback

--- node_helper.js
+++ node_helper.js
@@ -6,25 +6,30 @@ var defaultModules = require(__dirname + "/../../modules/default/defaultmodules.
 var SimpleGit = require("simple-git");
 //TODO: consider var npm  = require("npm");
 var child_process = require("child_process");
-var deasync = require("deasync");
 
 module.exports = NodeHelper.create({
 	config: {},
 
-	init: function() {
+	loaded: function(callback) {
 		console.log("AutoInstall - check modules to install");
 
-		for (var m in config.modules) {
-			var module = config.modules[m];
-			if (!module.disabled) {
-				try {
-					this.installModule(module);
-				} catch(e) {
-					console.log("Error while checking/installing " + module);
-					console.log(e);
-				}
+		var self = this;
+		var modules = config.modules;
+
+		var installNextModule = function() {
+			if (modules.length > 0) {
+				var nextModule = modules[0];
+				self.installModule(nextModule, function() {
+					modules = modules.slice(1);
+					installNextModule();
+				});
+			} else {
+				// All modules are installed
+				callback();
 			}
-		}
+		};
+
+		installNextModule();
 	},
 
 	/* installModule(module)
@@ -32,50 +37,59 @@ module.exports = NodeHelper.create({
 	 *
 	 * argument module object - The module from config to check and install.
 	 */
-	installModule: function(module) {
-		var elements = module.module.split("/");
-		var moduleName = elements[elements.length - 1];
-		var moduleFolder =  __dirname + "/../../modules/" + module.module;
-
-		if (defaultModules.indexOf(moduleName) !== -1) {
-			// Don't install default modules
-			console.log("AutoInstall - skip default module " + moduleName);
-			return;
-		}
-
+	installModule: function(module, callback) {
 		try {
-			fs.accessSync(moduleFolder, fs.R_OK);
-			// No exception, so dir exists
-			console.log("AutoInstall - already installed " + moduleName);
-			return;
-		} catch (e) {
-			//console.log(e);
-			console.log("Missing module: " + moduleName + " (" + moduleFolder + ".");
-		}
-
-		if (module.repository === undefined) {
-			// No repository configured
-			console.log("AutoInstall - skip module without repository " + moduleName);
-			return;
-		}
-		console.log("Install "+moduleName+" from "+module.repository);
-
-		var done = false;
-		var self = this;
-		SimpleGit().clone(module.repository, moduleFolder, function(err, data) {
-			if (err !== null) {
-				console.log(err);
-				console.log(err.stack);
-				done = true;
+			if (module.disabled) {
+				callback();
 				return;
 			}
 
-			self.npm_install(moduleFolder);
-			done = true;
-		});
+			var elements = module.module.split("/");
+			var moduleName = elements[elements.length - 1];
+			var moduleFolder =  __dirname + "/../../modules/" + module.module;
 
-		while(done === false) {
-			deasync.sleep(100);
+			if (defaultModules.indexOf(moduleName) !== -1) {
+				// Don't install default modules
+				console.log("AutoInstall - skip default module " + moduleName);
+				callback();
+				return;
+			}
+
+			try {
+				fs.accessSync(moduleFolder, fs.R_OK);
+				// No exception, so dir exists
+				console.log("AutoInstall - already installed " + moduleName);
+				callback();
+				return;
+			} catch (e) {
+				//console.log(e);
+				console.log("Missing module: " + moduleName + " (" + moduleFolder + ".");
+			}
+
+			if (module.repository === undefined) {
+				// No repository configured
+				console.log("AutoInstall - skip module without repository " + moduleName);
+				callback();
+				return;
+			}
+			console.log("Install "+moduleName+" from "+module.repository);
+
+			var self = this;
+			SimpleGit().clone(module.repository, moduleFolder, function(err, data) {
+				if (err !== null) {
+					console.log(err);
+					console.log(err.stack);
+					callback();
+					return;
+				}
+
+				self.npm_install(moduleFolder);
+				callback();
+			});
+		} catch(e) {
+			console.log("Error while checking/installing " + module);
+			console.log(e);
+			callback();
 		}
 	},
 
